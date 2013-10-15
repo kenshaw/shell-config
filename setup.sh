ef958cad57ff5bbd5e4cc5027674fda21b283648
Kenneth Shaw <kenshaw@gmail.com>
2013-10-15 15:29:47 +0700

    Minor fix

--- setup.sh
+++ setup.sh
@@ -2,9 +2,9 @@
 
 SRC="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-for i in `ls $SRC/env`; do 
-  if [[ -f $i && ! -e ~/.$i ]]; then
+for i in `ls $SRC/env`; do
+  if [[ ! -f $i && ! -e ~/.$i ]]; then
     echo "Linking: $SRC/env/$i to ~/.$i"
     ln -s $SRC/env/$i ~/.$i
-  fi 
+  fi
 done

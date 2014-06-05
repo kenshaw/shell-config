be4efba616528e16f6277bbc4bcb7e0ddc2bd685
Kenneth Shaw <kenshaw@gmail.com>
2014-06-05 11:02:45 +0700

    Adding some notes to vbm-osx script

--- misc/vbm-osx.sh
+++ misc/vbm-osx.sh
@@ -4,3 +4,9 @@ VM="$1"
 
 VBoxManage setextradata "$VM" VBoxInternal2/EfiGopMode 2
 VBoxManage setextradata "$VM" VBoxInternal2/SmcDeviceKey 'ourhardworkbythesewordsguardedpleasedontsteal(c)AppleComputerInc'
+
+# from: http://martinml.com/en/how-to-install-mac-os-x-snow-leopard-in-virtualbox/
+# after install, do this on OS X guest (and then reboot):
+
+# sudo rm /System/Library/Extensions/AppleIntelCPUPowerManagement.kext
+# sudo kextcache -system-caches

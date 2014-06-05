38561c180600c6fedbade4ce869ca7642dbdfe91
Kenneth Shaw <kenshaw@gmail.com>
2014-06-05 08:01:53 +0700

    Adding a chrome reorder script to use as a hotkey

---
+++ misc/xdotool-reorder-chrome.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+WINDOWS=$(xdotool search --onlyvisible --class "chrome")
+
+if [ -z "$WINDOWS" ]; then
+  exit 0
+fi
+
+FWIN=$(echo $WINDOWS|cut -d" " -f1)
+
+POSX=10
+for i in $WINDOWS; do
+  xdotool windowmove $i $POSX 35
+  POSX=$((POSX = POSX + 35))
+done

eaf125e8a86587ad444ea64e372abe9a90bbc6b0
Kenneth Shaw <kenshaw@gmail.com>
2014-06-06 16:46:30 +0700

    Adding ignore for Play mini player to chrome-reorder.sh script

--- xdo/chrome-reorder.sh
+++ xdo/chrome-reorder.sh
@@ -10,6 +10,9 @@ FWIN=$(echo $WINDOWS|cut -d" " -f1)
 
 POSX=10
 for i in $WINDOWS; do
-  xdotool windowmove $i $POSX 35
-  POSX=$((POSX = POSX + 35))
+  NAME=$(xdotool getwindowname $i)
+  if [[ ! "$NAME" =~ 'Google Play Music Mini Player' ]]; then
+    xdotool windowmove $i $POSX 35
+    POSX=$((POSX = POSX + 35))
+  fi
 done

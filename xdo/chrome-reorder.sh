93e12c2ca8992218778b0c8912b849b8cfa079c2
Kenneth Shaw <kenshaw@gmail.com>
2014-06-25 18:57:20 +0700

    Adding google keep to window ignore list for chrome reorder script

--- xdo/chrome-reorder.sh
+++ xdo/chrome-reorder.sh
@@ -33,7 +33,7 @@ fi
 # loop through windows
 for i in $WINDOWS; do
   NAME=$(xdotool getwindowname $i)
-  if [[ ! "$NAME" =~ 'Google Play Music Mini Player' ]]; then
+  if [[ ! "$NAME" =~ 'Google Play Music Mini Player' && ! "$NAME" =~ 'Google Keep' ]]; then
     # resize the window, move it to the right position, raise it, and then focus
     xdotool windowactivate $i windowsize $i $SIZEX $SIZEY windowmove $i $POSX $POSY windowraise $i windowfocus --sync $i
     POSX=$((POSX + SPACING))

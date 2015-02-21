e4ac6f0cc967c942c95477eabe0e5d91145c488b
Kenneth Shaw <kenshaw@gmail.com>
2015-02-21 08:22:10 +0700

    Adding icdiff to setup.sh and a git sdiff alias (to icdiff)

--- setup.sh
+++ setup.sh
@@ -2,11 +2,20 @@
 
 SRC="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-for i in `ls $SRC/env`; do
+for i in $(ls $SRC/env); do
   if [[ ! -f $i && ! -e ~/.$i ]]; then
     echo "Linking: $SRC/env/$i to ~/.$i"
     ln -s $SRC/env/$i ~/.$i
   fi
 done
+
+if [[ ! -d ~/src/icdiff ]]; then
+  echo "Checking out icdiff"
+
+  pushd ~/src > /dev/null
+  git clone https://github.com/jeffkaufman/icdiff.git
+
+  popd > /dev/null
+fi
 
 lesskey

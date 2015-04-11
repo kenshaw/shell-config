a118fd0e3a0893d14ae656505ac32998dc9a78b6
Kenneth Shaw <kenshaw@gmail.com>
2015-04-11 08:31:12 +0700

    Changing output of netblock.sh

--- scripts/netblock.sh
+++ scripts/netblock.sh
@@ -7,6 +7,6 @@ INETNUM=$(whois $IP|sed -n 's/^\(inetnum\|netrange\):\s*\([0-9\.]\+\s*-\s*[0-9\.
 if [ -z "${INETNUM}" ]; then
   echo -n "UNKNOWN"
 else
-  NETMASK=$(ipcalc $INETNUM|tail -n +2|sed ':a;N;$!ba;s/\n/, /g')
+  NETMASK=$(ipcalc $INETNUM|tail -n +2|sed ':a;N;$!ba;s/\n/ /g')
   echo -n $NETMASK
 fi

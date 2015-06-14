093b000cb7bff3847ea36b026caf75f7b9ba42ce
Kenneth Shaw <kenshaw@gmail.com>
2015-06-14 14:14:00 +0700

    Updating google-chrome-vpn.sh

--- scripts/google-chrome-vpn.sh
+++ scripts/google-chrome-vpn.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-google-chrome --user-data-dir="/home/$USER/.config/google-chrome-vpn" --proxy-server="socks5://127.0.0.1:8080"
+CHROME_DIR="/home/$USER/.config/google-chrome-vpn"
+
+rm -rf "$CHROME_DIR"
+google-chrome --user-data-dir="$CHROME_DIR" --proxy-server="socks5://localhost:8080" --no-first-run --window-size=1500,1060 --window-position=10,32 'http://ifconfig.me/all'

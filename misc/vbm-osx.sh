9f57520a42a33071ef5e87f57e2c7c04e9089b97
Kenneth Shaw <kenshaw@gmail.com>
2015-07-23 08:30:09 +0700

    Adding minor comment to vbm-osx.sh

--- misc/vbm-osx.sh
+++ misc/vbm-osx.sh
@@ -5,6 +5,7 @@ VM="$1"
 VBoxManage setextradata "$VM" VBoxInternal2/EfiGopMode 2
 VBoxManage setextradata "$VM" VBoxInternal2/SmcDeviceKey 'ourhardworkbythesewordsguardedpleasedontsteal(c)AppleComputerInc'
 
+# if booting, and its a black screen, the assigned ram amount is too high (must be 3 gigs or less)
 # from: http://martinml.com/en/how-to-install-mac-os-x-snow-leopard-in-virtualbox/
 # after install, do this on OS X guest (and then reboot):
 

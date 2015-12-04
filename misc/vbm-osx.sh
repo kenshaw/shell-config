f5a4ce1a1510f95705c5d98cc827c71747be101d
Kenneth Shaw <kenshaw@gmail.com>
2015-12-04 10:39:55 +0700

    Adding some comments from non-working VirtualBox config for OSX hosts

--- misc/vbm-osx.sh
+++ misc/vbm-osx.sh
@@ -5,6 +5,13 @@ VM="$1"
 VBoxManage setextradata "$VM" VBoxInternal2/EfiGopMode 2
 VBoxManage setextradata "$VM" VBoxInternal2/SmcDeviceKey 'ourhardworkbythesewordsguardedpleasedontsteal(c)AppleComputerInc'
 
+#VBoxManage modifyvm     "$VM" --cpuidset 00000001 000306a9 04100800 7fbae3ff bfebfbff
+#VBoxManage setextradata "$VM" "VBoxInternal/Devices/efi/0/Config/DmiSystemProduct" "MacBookPro11,3"
+#VBoxManage setextradata "$VM" "VBoxInternal/Devices/efi/0/Config/DmiSystemVersion" "1.0"
+#VBoxManage setextradata "$VM" "VBoxInternal/Devices/efi/0/Config/DmiBoardProduct" "Iloveapple"
+#VBoxManage setextradata "$VM" "VBoxInternal/Devices/smc/0/Config/DeviceKey" "ourhardworkbythesewordsguardedpleasedontsteal(c)AppleComputerInc"
+#VBoxManage setextradata "$VM" "VBoxInternal/Devices/smc/0/Config/GetKeyFromRealSMC" 1
+
 # if booting, and its a black screen, the assigned ram amount is too high (must be 3 gigs or less)
 # from: http://martinml.com/en/how-to-install-mac-os-x-snow-leopard-in-virtualbox/
 # after install, do this on OS X guest (and then reboot):

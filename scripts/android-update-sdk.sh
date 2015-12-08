39ace444603f64337c767196a184b9e1d2129e7b
Kenneth Shaw <kenshaw@gmail.com>
2015-12-08 17:58:34 +0700

    Fixing android-update-sdk.sh to handle errors better

--- scripts/android-update-sdk.sh
+++ scripts/android-update-sdk.sh
@@ -6,7 +6,9 @@ SDK_PAGE="https://developer.android.com/sdk/index.html"
 SDK_DLDIR="http://dl.google.com/android"
 SDK_REGEX="android-sdk_r[0-9\.]\+-linux.tgz"
 
-SDK_EXCL="doc- sample- sys-img- extra-android-m2repository extra-google-m2repository extra-android-gapid extra-google-auto"
+# exclude and include packages
+SDK_INCL=""
+SDK_EXCL="doc- sample- sys-img- extra-android-gapid extra-google-auto addon-google_gdk- extra-android-m2repository"
 
 DEST_DIR=$1
 if [ -z "${DEST_DIR}" ]; then
@@ -18,8 +20,17 @@ if [ ! -d "${DEST_DIR}" ]; then
   exit 1
 fi
 
+# hackish so as to not need to continually redownload build-tools
+if [ ! -d "${DEST_DIR}/android-sdk-linux/build-tools/23.0.1" ]; then
+  SDK_INCL="build-tools-23.0.1 ${SDK_INCL}"
+fi
+
+if [ ! -d "${DEST_DIR}/android-sdk-linux/extras/android/m2repository" ]; then
+  SDK_INCL="extra-android-m2repository ${SDK_INCL}"
+fi
+
 # reformat SDK_EXCL
-SDK_EXCL="^($(tr ' ' '|' <<< "$SDK_EXCL"))"
+SDK_EXCL="^\\($(sed -e 's/\s\+/\\|/g' <<< "$SDK_EXCL")\\)"
 
 NL=$'\n'
 
@@ -45,18 +56,22 @@ fi
 ANDROID_CMD="${DEST_DIR}/android-sdk-linux/tools/android"
 
 # get available packages, minus excluded
-PKGS_ALL=$($ANDROID_CMD list sdk --extended|sed -n 's/^id:\s*[0-9]\+\s*or\s*"\([^"]\+\)"/\1/p'|egrep -v $SDK_EXCL)
+PKGS_ALL=$($ANDROID_CMD list sdk --extended|sed -n 's/^id:\s*[0-9]\+\s*or\s*"\([^"]\+\)"/\1/p'|sed -n "/${SDK_EXCL}/ !p")
 
 # select only versions >= SDK_MIN
 PKGS_OTHER=$(egrep -v '(android|google)-[0-9]+$' <<< "$PKGS_ALL")
-PKGS_ANDROID=$(egrep '(android|google)-[0-9]+$' <<< "$PKGS_ALL"|awk -F- "(\$NF--)>${SDK_MIN}"|tr ' ' '-')
-PKGS_INSTALL=$(tr '\n' , <<< "${PKGS_ANDROID}${NL}${PKGS_OTHER}" |sed -e 's/,$//')
+PKGS_ANDROID=$(egrep '(android|google)-[0-9]+$' <<< "$PKGS_ALL"|awk -F- "(\$NF>=${SDK_MIN})"|tr ' ' '-')
 
-# update packages
-expect -c "
-set timeout -1;
-spawn $ANDROID_CMD update sdk --no-ui --filter ${PKGS_INSTALL};
-expect {
-  \"Do you accept the license\" { exp_send \"y\r\" ; exp_continue }
-  eof
-}"
+# generate list of all install packages
+PKGS_INSTALL=$(tr ' ' '\n' <<< "${PKGS_ANDROID}${NL}${PKGS_OTHER}${NL}${SDK_INCL}"|sed -e '/^\s*$/d'|tr '\n' ','|sed -e 's/,$//')
+
+if [ ! -z "$PKGS_INSTALL" ]; then
+  # update packages
+  expect -c "
+  set timeout -1;
+  spawn $ANDROID_CMD update sdk --no-ui --all --filter ${PKGS_INSTALL};
+  expect {
+    \"Do you accept the license\" { exp_send \"y\r\" ; exp_continue }
+    eof
+  }"
+fi

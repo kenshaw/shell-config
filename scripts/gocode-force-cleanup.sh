07bc23ee0731efcd9fd11267b0366efc44e4d4c4
Kenneth Shaw <kenshaw@gmail.com>
2016-07-05 10:54:37 +0700

    Adding gometalinter cleanup to gocode cleanup script

--- scripts/gocode-force-cleanup.sh
+++ scripts/gocode-force-cleanup.sh
@@ -10,10 +10,13 @@ killall -9 gocode
 # remove built binaries/packages
 rm -rf $HOME/src/go/pkg/*
 rm -rf $HOME/src/go/bin/gocode
+rm -rf $HOME/src/go/bin/gometalinter
 
 # cleanup crap in /tmp
 rm -rf /tmp/go-*
 rm -rf /tmp/gocode-daemon.*
 
-# reinstall gocode
+# reinstall
 go get -u github.com/nsf/gocode
+go get -u github.com/alecthomas/gometalinter
+gometalinter --install --update

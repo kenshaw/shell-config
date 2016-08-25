3b06e92331d7458b7c6f1987d8c9e1c40070da2d
Kenneth Shaw <kenshaw@gmail.com>
2016-08-25 08:24:58 +0700

    Adding chrome-api-keys.sh

---
+++ scripts/chrome-api-keys.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# taken from http://contribsoft.caixamagica.pt/browser/packages/anvil/chromium-browser/cmiffy/debdiff.patch
+
+export GOOGLEAPI_APIKEY_UBUNTU=AIzaSyAQfxPJiounkhOjODEO5ZieffeBv6yft2Q
+export GOOGLEAPI_CLIENTID_UBUNTU=424119844901.apps.googleusercontent.com
+export GOOGLEAPI_CLIENTSECRET_UBUNTU=AIienwDlGIIsHoKnNHmWGXyJ

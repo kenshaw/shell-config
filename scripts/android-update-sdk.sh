953af59e543887dd17c8f4fa2ff70d5abe7d9801
Kenneth Shaw <kenshaw@gmail.com>
2016-10-25 10:51:28 +0700

    Fixing issues with android-update-sdk.sh

--- scripts/android-update-sdk.sh
+++ scripts/android-update-sdk.sh
@@ -12,6 +12,10 @@ SDK_REGEX="android-sdk_r[0-9\.]\+-linux\.tgz"
 AWKBIN=awk
 SEDBIN=sed
 
+set -e
+
+NL=$'\n'
+
 # change paths for osx
 if [[ "$PLATFORM" == "Darwin" ]]; then
   SDK_DIR="android-sdk-macosx"
@@ -20,10 +24,6 @@ if [[ "$PLATFORM" == "Darwin" ]]; then
   AWKBIN=gawk
   SEDBIN=gsed
 fi
-
-# exclude and include packages
-SDK_INCL=""
-SDK_EXCL="doc- sample- sys-img- extra-android-gapid extra-google-auto addon-google_gdk- extra-google-simulators extra-intel- extra-android-m2repository"
 
 DEST_DIR=$1
 if [ -z "${DEST_DIR}" ]; then
@@ -38,26 +38,6 @@ if [ ! -d "${DEST_DIR}" ]; then
   echo "INVALID DESTINATION: ${DEST_DIR}"
   exit 1
 fi
-
-# hackish so as to not to continually redownload certain repos
-if [ ! -d "${DEST_DIR}/${SDK_DIR}/build-tools/23.0.1" ]; then
-  SDK_INCL="build-tools-23.0.1 ${SDK_INCL}"
-fi
-
-if [ ! -d "${DEST_DIR}/${SDK_DIR}/build-tools/23.0.2" ]; then
-  SDK_INCL="build-tools-23.0.2 ${SDK_INCL}"
-fi
-
-if [ ! -d "${DEST_DIR}/${SDK_DIR}/extras/android/m2repository" ]; then
-  SDK_INCL="extra-android-m2repository ${SDK_INCL}"
-fi
-
-# reformat SDK_EXCL
-SDK_EXCL="^\\($($SEDBIN -e 's/\s\+/\\|/g' <<< "$SDK_EXCL")\\)"
-
-NL=$'\n'
-
-set -ex
 
 if [ ! -d  "${DEST_DIR}/${SDK_DIR}" ]; then
   # get the sdk latest version
@@ -86,6 +66,32 @@ fi
 
 ANDROID_CMD="${DEST_DIR}/${SDK_DIR}/tools/android"
 
+# exclude and include packages
+SDK_INCL=""
+SDK_EXCL="doc- sample- sys-img- extra-android-gapid extra-google-auto addon-google_gdk- extra-google-simulators extra-intel-"
+
+# include extra build tool versions
+for VER in 23.0.1 23.0.2 25.0.0; do
+  if [ ! -d "${DEST_DIR}/${SDK_DIR}/build-tools/$VER" ]; then
+    SDK_INCL="build-tools-$VER ${SDK_INCL}"
+  fi
+done
+
+# exclude extra-android-m2repository if available <= installed
+SUPPORT_VER=$($ANDROID_CMD list sdk --extended |egrep 'Android Support Repository, revision'|$SEDBIN -e 's/.*revision\s*\(.*\)/\1/')
+SUPPORT_VER=$(($SUPPORT_VER+0))
+SUPPORT_MANIFEST="${DEST_DIR}/${SDK_DIR}/extras/android/m2repository/source.properties"
+if [ -f "${SUPPORT_MANIFEST}" ]; then
+  INST_VER=$(sed -n 's/^Pkg\.Revision=\([0-9]\+\).*/\1/p' $SUPPORT_MANIFEST)
+  INST_VER=$(($INST_VER+0))
+  if ((  $SUPPORT_VER <= $INST_VER )); then
+    SDK_EXCL="extra-android-m2repository ${SDK_EXCL}"
+  fi
+fi
+
+# reformat SDK_EXCL
+SDK_EXCL="^\\($($SEDBIN -e 's/\s\+/\\|/g' <<< "$SDK_EXCL")\\)"
+
 # get available packages, minus excluded
 PKGS_ALL=$($ANDROID_CMD list sdk --extended|$SEDBIN -n 's/^id:\s*[0-9]\+\s*or\s*"\([^"]\+\)"/\1/p'|$SEDBIN -n "/${SDK_EXCL}/ !p")
 
@@ -97,12 +103,20 @@ PKGS_ANDROID=$(egrep '(android|google)-[0-9]+$' <<< "$PKGS_ALL"|$AWKBIN -F- "(\$
 PKGS_INSTALL=$(tr ' ' '\n' <<< "${PKGS_ANDROID}${NL}${PKGS_OTHER}${NL}${SDK_INCL}"|$SEDBIN -e '/^\s*$/d'|tr '\n' ','|$SEDBIN -e 's/,$//')
 
 if [ ! -z "$PKGS_INSTALL" ]; then
-  # update packages
-  expect -c "
-  set timeout -1;
-  spawn $ANDROID_CMD update sdk --no-ui --all --filter ${PKGS_INSTALL};
-  expect {
-    \"Do you accept the license\" { exp_send \"y\r\" ; exp_continue }
-    eof
-  }"
+  echo "INSTALL: ${PKGS_INSTALL}"
+
+  # only if --update=yes
+  if [ "$1" == "--update=yes" ]; then
+    # update packages
+    expect -c "
+    set timeout -1;
+    spawn $ANDROID_CMD update sdk --no-ui --all --filter ${PKGS_INSTALL};
+    expect {
+      \"Do you accept the license\" { exp_send \"y\r\" ; exp_continue }
+      eof
+    }"
+
+  else
+    echo "NOTHING DONE! -- try --update=yes"
+  fi
 fi

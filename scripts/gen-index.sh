a6401f8f5e47b9c4dce4978b530f63e70b791268
Kenneth Shaw <kenshaw@gmail.com>
2016-11-13 08:22:08 +0700

    Updating gen-index.sh to handle output a bit better

--- scripts/gen-index.sh
+++ scripts/gen-index.sh
@@ -6,6 +6,20 @@ if [ ! -z "$1" ]; then
   DIR=$1
 fi
 
+if [[ ! "$DIR" =~ \/$ ]]; then
+  DIR="${DIR}/"
+fi
+
+IDX=$2
+if [ -z "$IDX" ]; then
+  IDX="/index.html"
+fi
+
+if [[ ! "$IDX" =~ ^\/ ]]; then
+  IDX="/${IDX}"
+fi
+
 echo '<html><body>'
-ls $DIR|sed 's/^.*/<a href="&">&<\/a><br\/>/'
+find $DIR -maxdepth 1 -type d -not -name \.\* |sort |grep -oP "^$DIR\K.*" |sed -e "s#^.*#<a href=\"&${IDX}\">& [DIR]</a><br/>#"
+find $DIR -maxdepth 1 -type f -not -name \.\* -not -name index.html|sort |grep -oP "^$DIR\K.*" |sed -e "s#^.*#<a href=\"&\">&</a><br/>#"
 echo '</body></html>'

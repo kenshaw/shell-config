4d1513bcbe1fe2f2221e790ba021c79af7c43eb0
Kenneth Shaw <kenshaw@gmail.com>
2019-08-13 05:44:35 +0700

    Updating config

--- osx/osx-vbm.sh
+++ osx/osx-vbm.sh
@@ -11,15 +11,16 @@ SMC_DEVICE_KEY='ourhardworkbythesewordsguardedpleasedontsteal(c)AppleComputerInc
 
 set -ex
 
-VBoxManage setextradata "$VM" VBoxInternal2/EfiGopMode 2
-VBoxManage setextradata "$VM" VBoxInternal2/SmcDeviceKey "$SMC_DEVICE_KEY"
-
-VBoxManage modifyvm     "$VM" --cpuidset 00000001 000306a9 04100800 7fbae3ff bfebfbff
-VBoxManage setextradata "$VM" "VBoxInternal/Devices/efi/0/Config/DmiSystemProduct" "MacBookPro11,3"
+VBoxManage modifyvm "$VM" --cpuidset 00000001 000106e5 00100800 0098e3fd bfebfbff
+VBoxManage setextradata "$VM" "VBoxInternal/Devices/efi/0/Config/DmiSystemProduct" "MacPro6,1"
 VBoxManage setextradata "$VM" "VBoxInternal/Devices/efi/0/Config/DmiSystemVersion" "1.0"
-VBoxManage setextradata "$VM" "VBoxInternal/Devices/efi/0/Config/DmiBoardProduct" "Mac-2BD1B31983FE1663"
+VBoxManage setextradata "$VM" "VBoxInternal/Devices/efi/0/Config/DmiBoardProduct" "Iloveapple"
 VBoxManage setextradata "$VM" "VBoxInternal/Devices/smc/0/Config/DeviceKey" "$SMC_DEVICE_KEY"
 VBoxManage setextradata "$VM" "VBoxInternal/Devices/smc/0/Config/GetKeyFromRealSMC" 1
+VBoxManage setextradata "$VM" VBoxInternal2/EfiGopMode 5
+VBoxManage setextradata "$VM" VBoxInternal2/EfiGraphicsResolution 1920x1080
+VBoxManage setextradata "$VM" VBoxInternal2/SmcDeviceKey "$SMC_DEVICE_KEY"
+VBoxManage setextradata "$VM" "VBoxInternal/Devices/efi/0/LUN#0/Config/PermanentSave" 1
 
 # if booting, and its a black screen, the assigned ram amount is too high (must be 3 gigs or less)
 # from: http://martinml.com/en/how-to-install-mac-os-x-snow-leopard-in-virtualbox/

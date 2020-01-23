40ecffcc027376fbc773793b8d108e5a9c858ab5
Kenneth Shaw <kenshaw@gmail.com>
2020-01-23 09:06:28 +0700

    Updating clean-kernels.sh

--- scripts/clean-kernels.sh
+++ scripts/clean-kernels.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-KERNEL=$(uname -r | sed -e 's/-\(generic\|amd64\)$//')
-REMOVE=$(dpkg --get-selections |egrep '^linux-.*-[0-9]\.[0-9]+\.[0-9]+-[0-9]+' | sed -e 's/\s*\(install\|deinstall\|purge\)$//' | grep -v $KERNEL| tr "\\n" " ")
+CURRENT=$(uname -r | sed -e 's/-\(generic\|amd64\)$//')
+GENERIC=$(apt-cache depends linux-image-generic|grep '^  Depends:'|awk -F: '{print $2}'|sed -e 's/\s//g'|grep 'linux-image-'|cut -d- -f 3,4)
+REMOVE=$(dpkg --get-selections | \
+  egrep '^linux-.*-[0-9]\.[0-9]+\.[0-9]+-[0-9]+' | \
+  sed -e 's/\s*\(install\|deinstall\|purge\)$//' | \
+  grep -v $CURRENT | \
+  grep -v $GENERIC | \
+  sed -e ':a;N;$!ba;s/\n/_ /g')
 
 set -e
 
-echo "KERNEL: $KERNEL"
-echo "REMOVE: $REMOVE"
+echo "CURRENT: $CURRENT"
+echo "GENERIC: $GENERIC"
+echo "REMOVE:  $REMOVE"
 
 if [ "$USER" != "root" ]; then
   echo "not root!"
@@ -14,7 +21,9 @@ if [ "$USER" != "root" ]; then
 fi
 
 if [ "$1" == "--clean=yes" ]; then
-  aptitude purge $REMOVE
+  (set -x;
+    aptitude install linux-generic linux-image-generic linux-headers-generic $REMOVE
+  )
 else
   echo "NOTHING DONE! -- try --clean=yes"
 fi

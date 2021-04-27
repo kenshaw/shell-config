18d2c2573b586a4c997cca92896f6702e6758cf0
Kenneth Shaw <kenshaw@gmail.com>
2021-04-27 08:50:28 +0700

    Adding backup.sh

---
+++ scripts/backup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+rsync -avP \
+  --exclude .steam* \
+  --exclude .cache \
+  --exclude .rustup \
+  --exclude .cargo \
+  --exclude .opam \
+  --exclude .local \
+  --exclude .wine \
+  --exclude .npm \
+  --exclude .dbus \
+  --exclude .haxe \
+  --exclude .npm-global \
+  --exclude .mozilla \
+  --exclude .vscode \
+  --exclude .config/unity3d \
+  --exclude .config/Slack \
+  --exclude src/qemu \
+  --exclude src/go/pkg \
+  --exclude src/go/bin \
+  --exclude src/infra/oracle-docker-images \
+  --delete \
+  /home/ken/. \
+  /media/wd2/backup/ken-desktop-current/.

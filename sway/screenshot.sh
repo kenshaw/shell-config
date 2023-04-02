4b5b86109d33bdacb316807ad562754c723db5f6
Kenneth Shaw <kenshaw@gmail.com>
2023-04-03 05:35:44 +0700

    Minor cleanup to screenshot.sh

--- sway/screenshot.sh
+++ sway/screenshot.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-OUT=$HOME/Pictures/screenshots
+CROP=0
 DATE=$(date +%Y-%m-%d_%H:%M:%S)
-CROP=
+OUT=$HOME/Pictures/screenshots
 
 OPTIND=1
 while getopts "gt:o:" opt; do
@@ -18,21 +18,25 @@ set -e
 mkdir -p $OUT/original
 
 SS=$OUT/original/ss-$DATE.png
+CROPPED=$OUT/original/ss-$DATE.cropped.png
+COORDSOUT=$OUT/original/ss-$DATE.cropped.txt
 FINAL=$OUT/ss-$DATE.png
 
 grim -c -l 0 $SS
 
-if [ ! -z "$CROP" ]; then
-  convert $SS -crop "$(slurp -f '%wx%h+%x+%y')" $SS.cropped
+if [[ "$CROP" == "1" ]]; then
+  COORDS="$(slurp -f '%wx%h+%x+%y')"
+  convert $SS -crop "$COORDS" $CROPPED
+  echo $COORDS|tee $COORDSOUT
 fi
 
 pngquant -o $SS.quantized $SS
 mv $SS.quantized $SS
 
 if [ ! -z "$CROP" ]; then
-  pngquant -o $SS.cropped.quantized $SS.cropped
-  mv $SS.cropped.quantized $SS.cropped
-  cp $SS.cropped $FINAL
+  pngquant -o $CROPPED.quantized $CROPPED
+  mv $CROPPED.quantized $CROPPED
+  cp $CROPPED $FINAL
 else
   cp $SS $FINAL
 fi

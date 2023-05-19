122c057540aa4a44127ed7a0452c451afd84c6e4
Kenneth Shaw <kenshaw@gmail.com>
2023-05-19 09:33:01 +0700

    Updating screenshot script

--- sway/screenshot.sh
+++ sway/screenshot.sh
@@ -1,25 +1,47 @@
 #!/bin/bash
 
+ANNOTATOR=com.github.phase1geo.annotator
+
+ANNOTATE=0
 CROP=0
 DATE=$(date +%Y-%m-%d_%H:%M:%S)
 OUT=$HOME/Pictures/screenshots
+UPLOAD=0
+EXPIRATION=$((7*24*3600))
 
 OPTIND=1
-while getopts "gt:o:" opt; do
+while getopts "agt:o:ue:" opt; do
 case "$opt" in
+  a) ANNOTATE=1 ;;
   g) CROP=1 ;;
   t) DATE=$OPTARG ;;
   o) OUT=$OPTARG ;;
+  u) UPLOAD=1 ;;
+  e) EXPIRATION=$OPTARG ;;
 esac
 done
+
+quantize() {
+  pngquant -o $1.quantized $1
+  mv $1.quantized $1
+}
 
 set -e
 
 mkdir -p $OUT/original
+mkdir -p $HOME/.config/ss
+
+if [ ! -e $HOME/.config/ss/ibb ]; then
+  echo "error: $HOME/.config/ss/ibb is missing!"
+  exit 1
+fi
+
+APIKEY=$(cat $HOME/.config/ss/ibb)
 
 SS=$OUT/original/ss-$DATE.png
 CROPPED=$OUT/original/ss-$DATE.cropped.png
 COORDSOUT=$OUT/original/ss-$DATE.cropped.txt
+ANNOTATED=$OUT/original/ss-$DATE.annotated.png
 FINAL=$OUT/ss-$DATE.png
 
 grim -c -l 0 $SS
@@ -27,16 +49,40 @@ grim -c -l 0 $SS
 if [[ "$CROP" == "1" ]]; then
   COORDS="$(slurp -f '%wx%h+%x+%y')"
   convert $SS -crop "$COORDS" $CROPPED
-  echo $COORDS|tee $COORDSOUT
+  echo "$COORDS" | tee -a $COORDSOUT &> /dev/null
 fi
 
-pngquant -o $SS.quantized $SS
-mv $SS.quantized $SS
+quantize $SS
+WORKING=$SS
 
-if [ ! -z "$CROP" ]; then
-  pngquant -o $CROPPED.quantized $CROPPED
-  mv $CROPPED.quantized $CROPPED
-  cp $CROPPED $FINAL
-else
-  cp $SS $FINAL
+if [[ "$CROP" == "1" ]]; then
+  quantize $CROPPED
+  WORKING=$CROPPED
 fi
+
+if [[ "$ANNOTATE" == "1" ]]; then
+  cp $WORKING $ANNOTATED
+  $ANNOTATOR $ANNOTATED
+  quantize $ANNOTATED
+  WORKING=$ANNOTATED
+fi
+
+cp $WORKING $FINAL
+
+if [[ "$UPLOAD" != "1" ]]; then
+  exit
+fi
+
+# upload to ibb
+RES=$(
+  curl \
+    --silent \
+    --location \
+    --request POST \
+    --form "image=$(base64 -w 0 < $FINAL)" \
+    "https://api.imgbb.com/1/upload?expiration=$EXPIRATION&key=$APIKEY"
+)
+#cat <<< "$RES"
+URL=$(jq -r .data.image.url <<< "$RES")
+xdg-open "$URL"
+wl-copy <<< "$URL"

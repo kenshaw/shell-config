dc1b38738d0f2789a2caaa63750b8875bd1298f8
Kenneth Shaw <kenshaw@gmail.com>
2023-10-03 07:33:24 +0700

    Fixing issues with Nerd Font 3.0

--- sway/logout.sh
+++ sway/logout.sh
@@ -5,7 +5,8 @@ killall -9 swaynag
 swaynag -t warning \
   -m 'Exit?' \
   -e bottom \
-  -z '' 'swaymsg exit' \
-  -z '' 'systemctl -i reboot' \
-  -z '⏻' 'systemctl -i poweroff' \
-  -z '' "$HOME/src/shell-config/sway/lock.sh"
+  -z ' 󰗼 ' 'swaymsg exit' \
+  -z '  ' 'systemctl -i reboot' \
+  -z ' ⏻ ' 'systemctl -i poweroff' \
+  -z ' 󰍁 ' "$HOME/src/shell-config/sway/lock.sh" \
+  -s ' 󰖭 '

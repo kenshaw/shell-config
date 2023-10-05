1e8c376300794b0f0e3986fe088ea52b74f1e957
Kenneth Shaw <kenshaw@gmail.com>
2023-10-05 07:39:35 +0700

    Updating sway/lock.sh

--- sway/lock.sh
+++ sway/lock.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+SRC="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 swayidle \
   timeout 10 'swaymsg "output * dpms off"' \
   resume 'swaymsg "output * dpms on"' &
@@ -7,5 +9,7 @@ swayidle \
 swaylock \
   --color 000000 \
   --image ~/Pictures/backgrounds/mountain-road-cropped.png
+
+$SRC/reorder.sh
 
 kill %%

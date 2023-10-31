418f2d06991ec4940f5af53823ee4a41f2968763
Kenneth Shaw <kenshaw@gmail.com>
2023-11-01 06:53:03 +0700

    Updating ge-grab.sh script to clean up dead files

--- scripts/ge-grab.sh
+++ scripts/ge-grab.sh
@@ -9,13 +9,15 @@ set -e
 
 mkdir -p $OUT
 
+CLEAN=0
 DOWNLOAD=0
 FORCE=
 VERSION=
 
 OPTIND=1
-while getopts "dfv:" opt; do
+while getopts "cdfv:" opt; do
 case "$opt" in
+  c) CLEAN=1 ;;
   d) DOWNLOAD=1 ;;
   f) FORCE=1 ;;
   v) VERSION=$OPTARG ;;
@@ -38,17 +40,16 @@ echo "VERSION:   $VERSION"
 echo "OUT:       ~${OUT#$HOME}"
 echo "INSTALLED: $(ls -d $OUT/*|grep -v '.tar.gz'|sed -e "s%$OUT/%%g"|xargs)"
 echo "DL:        $DL"
+echo "CLEAN:     $CLEAN"
 
-if [ "$DOWNLOAD" != "1" ]; then
-  exit
-fi
+if [ "$DOWNLOAD" = "1" ]; then
+  if [ "$FORCE" == "1" ]; then
+    rm -rf $OUT/$VERSION.tar.gz $OUT/$VERSION
+  fi
 
-if [ "$FORCE" == "1" ]; then
-  rm -rf $OUT/$VERSION.tar.gz $OUT/$VERSION
-fi
-
-if [ ! -f $OUT/$VERSION.tar.gz ]; then
-  curl -4 -L -# -o $OUT/$VERSION.tar.gz "$DL"
+  if [ ! -f $OUT/$VERSION.tar.gz ]; then
+    curl -4 -L -# -o $OUT/$VERSION.tar.gz "$DL"
+  fi
 fi
 
 CHECKSUM=$(curl -4 -L -s "$(printf "$SHA" $VERSION $VERSION)"|awk '{print $1}')
@@ -63,4 +64,17 @@ echo "SIGNATURE: valid"
 
 if [ ! -d $OUT/$VERSION ]; then
   tar -xf $OUT/$VERSION.tar.gz -C $OUT
+  rm -f $OUT/GE-Latest
+  ln -s ./$VERSION $OUT/GE-Latest
+fi
+
+if [ "$CLEAN" = "1" ]; then
+  REMOVE=$(cut -d- -f1-2 <<< "$VERSION")
+  (set -x;
+    find $OUT -maxdepth 1 -iname "$REMOVE*" -not -iname "$VERSION*" -exec rm -rf {} \;
+  )
+fi
+
+if [[ "$DOWNLOAD" == "1" || "$CLEAN" == "1" ]]; then
+  echo "INSTALLED: $(ls -d $OUT/*|grep -v '.tar.gz'|sed -e "s%$OUT/%%g"|xargs)"
 fi

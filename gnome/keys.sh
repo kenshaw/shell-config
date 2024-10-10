1405d7b5aef55c2e72eec7e0777bb681f7bf95ce
Kenneth Shaw <kenshaw@gmail.com>
2024-10-11 02:52:26 +0700

    Adding gnome/keys.sh script

---
+++ gnome/keys.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+gsettings set org.gnome.desktop.wm.keybindings switch-windows "['<Alt>Tab']"
+gsettings set org.gnome.desktop.wm.keybindings switch-windows-backward "['<Shift><Alt>Tab', '<Alt>Above_Tab']"
+gsettings set org.gnome.desktop.wm.keybindings switch-applications "[]"
+gsettings set org.gnome.desktop.wm.keybindings switch-applications-backward "[]"

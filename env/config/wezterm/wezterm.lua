0693fac7e4fc59372a6a3ca49afb737b06ca522b
Kenneth Shaw <kenshaw@gmail.com>
2024-10-25 05:00:20 +0700

    Adding wezterm configuration

---
+++ env/config/wezterm/wezterm.lua
@@ -0,0 +1,11 @@
+local wezterm = require 'wezterm'
+local config = {}
+
+--config.color_scheme = 'One Dark (Gogh)'
+--config.color_scheme = 'OneHalfDark'
+config.window_background_opacity = 0.5
+
+config.font = wezterm.font('Inconsolata LGC Nerd Font Mono')
+config.font_size = 28.0
+
+return config

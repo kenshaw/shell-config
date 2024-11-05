974df0569167b1e07fdbdd70e6b371e60fce382f
Kenneth Shaw <kenshaw@gmail.com>
2024-11-05 08:35:27 +0700

    Updating lock script

--- sway/lock.sh
+++ sway/lock.sh
@@ -6,9 +6,12 @@ swayidle \
   timeout 10 'swaymsg "output * dpms off"' \
   resume 'swaymsg "output * dpms on"' &
 
-swaylock \
-  --color 000000 \
-  --image ~/Pictures/backgrounds/mountain-road-cropped.png
+swaylock-plugin \
+  --command "mpvpaper -o 'no-audio --loop-playlist shuffle --speed=0.8 --osd-playing-msg=\"\${osd-ass-cc/0}{\\\\\\\\an2}\${osd-ass-cc/1}\${media-title}\"' '*' ~/Pictures/backgrounds/apple/wallpapers.m3u"
+
+#swaylock \
+#  --color 000000 \
+#  --image ~/Pictures/backgrounds/mountain-road-cropped.png
 
 $SRC/reorder.sh
 

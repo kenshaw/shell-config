6a7a13e5f3f4e6162ff0c0ad0b48385f8a17e571
Kenneth Shaw <kenshaw@gmail.com>
2024-11-06 04:08:53 +0700

    Fixing osd display on mpvpaper background

--- sway/lock.sh
+++ sway/lock.sh
@@ -6,12 +6,12 @@ swayidle \
   timeout 10 'swaymsg "output * dpms off"' \
   resume 'swaymsg "output * dpms on"' &
 
-swaylock-plugin \
-  --command "mpvpaper -o 'no-audio --loop-playlist shuffle --speed=0.8 --osd-playing-msg=\"\${osd-ass-cc/0}{\\\\\\\\an2}\${osd-ass-cc/1}\${media-title}\"' '*' ~/Pictures/backgrounds/apple/wallpapers.m3u"
+#swaylock-plugin \
+#  --command "mpvpaper -o 'no-audio --loop-playlist shuffle --speed=0.8 --osd-margin-y=70 --osd-playing-msg-duration=7500 --osd-playing-msg=\"\${osd-ass-cc/0}{\\\\\\\\an2}\${osd-ass-cc/1}\${media-title}\"' '*' ~/Pictures/backgrounds/apple/wallpapers.m3u"
 
-#swaylock \
-#  --color 000000 \
-#  --image ~/Pictures/backgrounds/mountain-road-cropped.png
+swaylock \
+  --color 000000 \
+  --image ~/Pictures/backgrounds/mountain-road-cropped.png
 
 $SRC/reorder.sh
 

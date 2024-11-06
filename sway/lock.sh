683bfe0a7b0fa9e86b7ef022f51d0f45af4211a5
Kenneth Shaw <kenshaw@gmail.com>
2024-11-06 14:10:44 +0700

    More sway lock config changes

--- sway/lock.sh
+++ sway/lock.sh
@@ -2,9 +2,13 @@
 
 SRC="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+mpvctl=$HOME/.local/lib/mpvpaper/control
+
 swayidle \
-  timeout 10 'swaymsg "output * dpms off"' \
-  resume 'swaymsg "output * dpms on"' &
+  timeout 2 'swaymsg "output * dpms off"' \
+  resume 'swaymsg "output * dpms on"' \
+  timeout 2 "soctl - $mpvctl <<< 'set pause yes'" \
+  resume "soctl - $mpvctl <<< 'set pause no'" &
 
 #swaylock-plugin \
 #  --command "mpvpaper -o 'no-audio --loop-playlist shuffle --speed=0.8 --osd-margin-y=70 --osd-playing-msg-duration=7500 --osd-playing-msg=\"\${osd-ass-cc/0}{\\\\\\\\an2}\${osd-ass-cc/1}\${media-title}\"' '*' ~/Pictures/backgrounds/apple/wallpapers.m3u"

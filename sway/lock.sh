d5a9dd25195e4e7641c4a679cd6c6a9962e5f4bb
Kenneth Shaw <kenshaw@gmail.com>
2024-11-07 10:41:04 +0700

    Fixing sway/lock.sh

--- sway/lock.sh
+++ sway/lock.sh
@@ -5,10 +5,11 @@ SRC="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 mpvctl=$HOME/.local/lib/mpvpaper/control
 
 swayidle \
+  -w \
   timeout 10 'swaymsg "output * dpms off"' \
   resume 'swaymsg "output * dpms on"' \
-  timeout 10 "soctl - $mpvctl <<< 'set pause yes'" \
-  resume "soctl - $mpvctl <<< 'set pause no'" &
+  timeout 10 "socat - $mpvctl <<< 'set pause yes'" \
+  resume "socat - $mpvctl <<< 'set pause no'" &
 
 #swaylock-plugin \
 #  --command "mpvpaper -o 'no-audio --loop-playlist shuffle --speed=0.8 --osd-margin-y=70 --osd-playing-msg-duration=7500 --osd-playing-msg=\"\${osd-ass-cc/0}{\\\\\\\\an2}\${osd-ass-cc/1}\${media-title}\"' '*' ~/Pictures/backgrounds/apple/wallpapers.m3u"
@@ -17,6 +18,6 @@ swaylock \
   --color 000000 \
   --image ~/Pictures/backgrounds/mountain-road-cropped.png
 
-$SRC/reorder.sh
+#$SRC/reorder.sh
 
 kill %%

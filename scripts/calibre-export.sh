0484e4b3f268572b0b490ecda6445e0359224d59
Kenneth Shaw <kenshaw@gmail.com>
2025-05-23 05:46:43 +0700

    Fixes to scripts/calibre-export.sh

--- scripts/calibre-export.sh
+++ scripts/calibre-export.sh
@@ -3,7 +3,17 @@
 LIBRARY="$HOME/Calibre Library"
 OUT=$HOME/epub
 
-FILES=$(find "$LIBRARY" -type f \( -iname \*.pdf -o -iname \*epub \) |grep -v 'original_epub')
+FILES=$(
+  find "$LIBRARY" \
+    -type f \
+    -not -path "*caltrash*" \
+    -and -not -path "*calnotes" \
+    -and -not -path "*original_epub*" \
+    -and \(  \
+      -iname \*.pdf \
+      -or -iname \*epub \
+    \)
+)
 
 (set -x;
   mkdir -p $OUT
@@ -14,7 +24,8 @@ while IFS= read -r line; do
   num=$(sed -e 's/.*(\([0-9]\+\))$/\1/' <<< "$dir")
   name=$(echo "$dir"|rev|cut -d' ' -f2-|rev|tr -c -d '[a-zA-Z0-9 ]'|cut -c1-64|xargs)
   ext="${line##*.}"
+  num=$(printf "%03d" "$num")
   (set -x;
-    cp -a "$line" "$OUT/$(printf "%03d" "$num") ${name}.${ext}"
+    cp -a "$line" "$OUT/$num ${name}.${ext}"
   )
 done <<< "$FILES"

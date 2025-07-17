19b4452145a5dff7969dab762f4f13e04f8ce632
Kenneth Shaw <kenshaw@gmail.com>
2025-07-17 08:48:59 +0700

    Fix to sway/keyboard-layout.sh

--- sway/keyboard-layout.sh
+++ sway/keyboard-layout.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 LAYOUT=$(swaymsg -t get_inputs \
-  |jq -r '[.[]|select(.type=="keyboard")|select(.name|contains("Keyboard"))][0].xkb_active_layout_name' \
+  |jq -r '[.[]|select(.type=="keyboard")][0].xkb_active_layout_name' \
   |cut -d '(' -f2 \
   |cut -d ')' -f1 \
   |tr '[A-Z]' '[a-z]')

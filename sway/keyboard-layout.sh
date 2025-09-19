93bce3ba8b5870d0c0755c277a82427aff30cbbd
Kenneth Shaw <kenshaw@gmail.com>
2025-09-19 07:26:01 +0700

    Minor cleanup to sway/keyboard-layout.sh

--- sway/keyboard-layout.sh
+++ sway/keyboard-layout.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-LAYOUT=$(swaymsg -t get_inputs \
-  |jq -r '.[]|try select(.xkb_layout_names[]| try contains("English (Dvorak)"))|.xkb_active_layout_name' \
-  |head -1 \
-  |cut -d '(' -f2 \
-  |cut -d ')' -f1 \
-  |tr '[A-Z]' '[a-z]')
+LAYOUT=$(
+  swaymsg -t get_inputs \
+    |jq -r '.[]|try select(.xkb_layout_names[]| try contains("English (Dvorak)"))|.xkb_active_layout_name' \
+    |head -1 \
+    |cut -d '(' -f2 \
+    |cut -d ')' -f1 \
+    |tr '[A-Z]' '[a-z]'
+)
 
 echo "${LAYOUT:0:2}"

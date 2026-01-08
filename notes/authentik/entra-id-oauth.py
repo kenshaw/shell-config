655df3f7fcd7ed0af776d5296109fe941400745d
Kenneth Shaw <kenshaw@gmail.com>
2026-01-08 13:15:37 +0700

    Updating entra-id-oauth.py

--- notes/authentik/entra-id-oauth.py
+++ notes/authentik/entra-id-oauth.py
@@ -1,10 +1,26 @@
+# determine email and username, validate
+email = info.get("mail")
+uname = email.split('@')[0]
+if len(uname) == 0:
+  raise Exception(f"invalid email {email}")
+if "admin" in uname.lower():
+  raise Exception(f"invalid user {uname}")
+
+# get first, last, name, validate
+name = info.get("displayName").strip()
+fname = info.get("givenName").strip()
+lname = info.get("surname").strip()
+
+if len(name) < 3:
+  raise Exception(f"invalid name '{name}'")
+
 return {
-    "uid": info.get("mail"),
-    "name": info.get("displayName"),
-    "username": info.get("mail").split('@')[0],
-    "email": info.get("mail"),
+    "uid": email,
+    "name": name,
+    "username": uname,
+    "email": email,
     "attributes": {
-      "firstname": info.get("givenName"),
-      "lastname": info.get("surname")
+      "firstname": fname,
+      "lastname": lname
     }
 }
